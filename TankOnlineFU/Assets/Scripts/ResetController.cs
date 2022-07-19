dae29acff20222b4609b276e9de67ecf4c14afdd
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-07-19 20:28:17 +0700

    add animation retry

--- TankOnlineFU/Assets/Scripts/ResetController.cs
+++ TankOnlineFU/Assets/Scripts/ResetController.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ResetController : MonoBehaviour
 {
+    public GameObject canvas;
+    public Image fade;
+    public Image tank;
+    private Animator tankAni;
+    private Animator fadeAni;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tankAni = tank.GetComponent<Animator>();
+        fadeAni = fade.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,7 +24,15 @@ public class ResetController : MonoBehaviour
         
     }
 
-    public void ResetGame()
+    public void ClickRetry()
+    {
+        canvas.SetActive(true);
+        tankAni.SetTrigger("retry");
+        fadeAni.SetTrigger("retry");
+        Invoke("ResetGame", 3f);
+    }
+
+    void ResetGame()
     {
         Score.SCORE_PLAYER1 = 0;
         Score.SCORE_PLAYER2 = 0;
